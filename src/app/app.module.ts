0cf7ab234c3aa72cf31dbedba8fc25cbc7ce7f8f
Matthew Rungwe <matthewrungwe@gmail.com>
2021-07-14 21:41:04 +0200

    Create all components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav-footer/nav/nav.component';
+import { FooterComponent } from './nav-footer/footer/footer.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { HireMeComponent } from './hire-me/hire-me.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    AboutMeComponent,
+    ContactComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    HireMeComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hire-me/hire-me.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hire-me',
+  templateUrl: './hire-me.component.html',
+  styleUrls: ['./hire-me.component.less']
+})
+export class HireMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
