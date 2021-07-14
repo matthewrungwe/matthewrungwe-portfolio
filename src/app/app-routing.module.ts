3547af5d4ac377c0d3a192f1c007775ce246867a
Matthew Rungwe <matthewrungwe@gmail.com>
2021-07-14 21:57:06 +0200

    Create routing for application.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
+// Imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { AboutMeComponent } from './about-me/about-me.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { HireMeComponent } from './hire-me/hire-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+
+const routes: Routes = [
+  { path: "about-me", component: AboutMeComponent },
+  { path: "resume", component: ResumeComponent },
+  { path: "portfolio", component: PortfolioComponent },
+  { path: "blog", component: BlogComponent },
+  { path: "hire-me", component: HireMeComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "", redirectTo: "about-me", pathMatch: "full"},
+  { path: "**", component: ErrorPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
